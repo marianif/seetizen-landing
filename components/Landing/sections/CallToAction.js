c4b48ff49d29c35d0ae2f2d463a49e9f374a6b2a
marianif <47568942+marianif@users.noreply.github.com>
2022-11-19 00:57:38 +0100

    add gradient

--- components/Landing/sections/CallToAction.js
+++ components/Landing/sections/CallToAction.js
@@ -60,7 +60,7 @@ const CallToAction = () => {
   // }
 
   return (
-    <div className={styles.container} id="cta">
+    <div className={`${styles.container} gradientBg`} id="cta">
       <a href="#cta">
         <h2 className="text-light">
           Registrati per la beta!

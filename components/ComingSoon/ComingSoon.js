d0c09586ae2c51168d78c222c99969786f9aa6a8
Federica Mariani <federica.mariani@enhancers.it>
2023-09-22 14:57:06 +0200

    add coming soon

--- components/ComingSoon/ComingSoon.js
+++ components/ComingSoon/ComingSoon.js
@@ -1,7 +1,20 @@
 import React from "react"
 
 const ComingSoon = () => {
-  return <div>ComingSoon</div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "80vh",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Stiamo rifacendo il sito, a breve torneremo online!</h1>
+    </div>
+  )
 }
 
 export default ComingSoon
